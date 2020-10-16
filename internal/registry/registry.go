3ce520666af5c98a23951fac78df7e156de8d21d
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-10-17 01:11:35 +0900

    added CreateLayer

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -1,6 +1,13 @@
 package registry
 
-import "path/filepath"
+import (
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/h2non/filetype"
+)
 
 // BasePath represents base path for this application.
 var BasePath = "testdata"
@@ -17,4 +24,29 @@ func PathJoinWithBase(name string, p ...string) string {
 			p...,
 		)...,
 	)
+}
+
+// CreateLayer creates layer a file which will be json or gz extension on specified path.
+func CreateLayer(r io.Reader, path string) (int64, error) {
+	// see filetype.MatchReader
+	buffer := make([]byte, 8192)
+	n, err := r.Read(buffer)
+	if err != nil && err != io.EOF {
+		return 0, err
+	}
+
+	filePath := filepath.Join(path, "layer"+detectExt(buffer))
+	f, err := os.Create(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	return io.Copy(f, io.MultiReader(bytes.NewReader(buffer[:n]), r))
+}
+
+func detectExt(buf []byte) string {
+	if filetype.IsArchive(buf) {
+		return ".tar.gz"
+	}
+	return ".json"
 }
