3bf1bbaa69ff4a2a277ade317a506e5b6a249ebe
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-10-17 22:59:08 +0900

    removed unused files

--- adapters.go
+++ adapters.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+// ServerAdapter represents a apply middleware type for http server.
+type ServerAdapter func(http.Handler) http.Handler
+
+// ServerApply applies http server middlewares
+func ServerApply(h http.Handler, adapters ...ServerAdapter) http.Handler {
+	// To process from left to right, iterate from the last one.
+	for i := len(adapters) - 1; i >= 0; i-- {
+		h = adapters[i](h)
+	}
+	return h
+}
+
 // AccessLogServerAdapter logs access log
 func AccessLogServerAdapter() ServerAdapter {
 	return func(next http.Handler) http.Handler {
